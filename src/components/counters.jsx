46f37f19d9bf1a70684bd0ab19d66864bcdd64bb
Chris Conner <chris.conner57@gmail.com>
2020-02-19 15:23:59 -0800

    Changed Folder

---
+++ src/components/counters.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+
+class Counters extends Component {
+  render() {
+    const {
+      onReset,
+      counters,
+      onDelete,
+      onIncrement,
+      onDecrement
+    } = this.props;
+
+    return (
+      <div>
+        {counters.map(counter => (
+          <Counter
+            key={counter.id}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
+            onDecrement={onDecrement}
+            counter={counter}
+          />
+        ))}
+        <button onClick={onReset} className="btn btn-primary btn-sml m-2">
+          Reset
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Counters;
